62005273eb3e76f7d31393b659d006712d6982be
Zachary Stence <zachstence@gmail.com>
2024-07-18 12:45:34 -0500

    feat: basic dnd

--- src/lib/Letter.svelte
+++ src/lib/Letter.svelte
@@ -1,3 +1,5 @@
-<div class="flex aspect-square h-16 w-16 items-center justify-center rounded-md border p-4">
+<div
+	class="flex aspect-square h-16 w-16 items-center justify-center rounded-md border bg-white p-4"
+>
 	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,40 @@
 <script lang="ts">
+	import { dndzone, type DndEvent } from 'svelte-dnd-action';
 	import Letter from '$lib/Letter.svelte';
+
+	type Item = {
+		id: string;
+	};
+
+	let items = [
+		{ id: 'A' },
+		{ id: 'B' },
+		{ id: 'C' },
+		{ id: 'D' },
+		{ id: 'E' },
+		{ id: 'F' },
+		{ id: 'G' },
+		{ id: 'H' },
+		{ id: 'I' },
+		{ id: 'J' },
+		{ id: 'K' },
+		{ id: 'L' }
+	];
+
+	const handleDndConsider = (e: CustomEvent<DndEvent<Item>>) => {
+		items = e.detail.items;
+	};
+	const handleDndFinalize = (e: CustomEvent<DndEvent<Item>>) => {
+		items = e.detail.items;
+	};
 </script>
 
-<Letter>A</Letter>
-<Letter>B</Letter>
-<Letter>C</Letter>
-<Letter>D</Letter>
-<Letter>E</Letter>
-<Letter>F</Letter>
-<Letter>G</Letter>
-<Letter>H</Letter>
-<Letter>I</Letter>
-<Letter>J</Letter>
-<Letter>K</Letter>
-<Letter>L</Letter>
+<div
+	use:dndzone={{ items, flipDurationMs: 200 }}
+	on:consider={handleDndConsider}
+	on:finalize={handleDndFinalize}
+>
+	{#each items as item (item.id)}
+		<Letter>{item.id}</Letter>
+	{/each}
+</div>
