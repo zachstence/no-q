338fd0d81771af975605d2f72c5179aba5b54b3c
Zachary Stence <zachstence@gmail.com>
2024-07-20 15:06:30 -0500

    fix: Cell letter snaps back to original spot

--- src/lib/Cell.svelte
+++ src/lib/Cell.svelte
@@ -18,22 +18,23 @@
 	};
 
 	$: considering = _item?.[SHADOW_ITEM_MARKER_PROPERTY_NAME];
+	$: items = _item ? [_item] : [];
 </script>
 
 <div
 	class="aspect-square h-12 w-12 rounded-md bg-stone-900 transition-colors duration-100"
 	class:bg-stone-600={considering}
 	use:dndzone={{
-		items: _item ? [_item] : [],
+		items,
 		dropFromOthersDisabled: typeof item !== 'undefined',
 		dropTargetStyle: {}
 	}}
 	on:consider={onConsider}
 	on:finalize={onFinalize}
 >
-	{#if _item}
+	{#each items as item (item.id)}
 		<Letter>
-			{_item.letter}
+			{item.letter}
 		</Letter>
-	{/if}
+	{/each}
 </div>
