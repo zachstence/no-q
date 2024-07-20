e5cf5770235442de0bb0aaeb90f893fb836a4b37
Zachary Stence <zachstence@gmail.com>
2024-07-20 15:09:36 -0500

    feat: multiple cells

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,24 @@
 	import type { Item } from '$lib/item';
 	import { nanoid } from 'nanoid';
 
-	const item: Item | undefined = {
+	const a: Item | undefined = {
 		id: nanoid(),
-		letter: 'X'
+		letter: 'a'
+	};
+	const b: Item | undefined = {
+		id: nanoid(),
+		letter: 'b'
 	};
 
-	let item1: Item | undefined = item;
-	let item2: Item | undefined = undefined;
+	let item1: Item | undefined = a;
+	let item2: Item | undefined = b;
+	let item3: Item | undefined = undefined;
 
-	$: console.log({ item1, item2 });
+	$: console.log({ item1, item2, item3 });
 </script>
 
 <div class="flex flex-row gap-4">
 	<Cell bind:item={item1} />
 	<Cell bind:item={item2} />
+	<Cell bind:item={item3} />
 </div>
