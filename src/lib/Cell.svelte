8ec54568c36cddaee2caeb7d7ef1c59a652c0790
Zachary Stence <zachstence@gmail.com>
2024-07-20 15:01:28 -0500

    feat: cell only sets item on drop

--- src/lib/Cell.svelte
+++ src/lib/Cell.svelte
@@ -1,22 +1,39 @@
 <script lang="ts">
-	import { dndzone } from 'svelte-dnd-action';
+	import { dndzone, type DndEvent, SHADOW_ITEM_MARKER_PROPERTY_NAME } from 'svelte-dnd-action';
 	import type { Item } from './item';
 	import Letter from './Letter.svelte';
 
 	export let item: Item | undefined = undefined;
+	let _item: Item | undefined = item;
 
-	$: items = item ? [item] : [];
+	type DnDHandler = (e: CustomEvent<DndEvent<Item>>) => void;
+
+	const onConsider: DnDHandler = (e) => {
+		_item = e.detail.items[0];
+	};
+
+	const onFinalize: DnDHandler = (e) => {
+		item = e.detail.items[0];
+		_item = item;
+	};
+
+	$: considering = _item?.[SHADOW_ITEM_MARKER_PROPERTY_NAME];
 </script>
 
 <div
 	class="aspect-square h-12 w-12 rounded-md bg-stone-900 transition-colors duration-100"
-	use:dndzone={{ items }}
-	on:consider={(e) => (item = e.detail.items[0])}
-	on:finalize={(e) => (item = e.detail.items[0])}
+	class:bg-stone-600={considering}
+	use:dndzone={{
+		items: _item ? [_item] : [],
+		dropFromOthersDisabled: typeof item !== 'undefined',
+		dropTargetStyle: {}
+	}}
+	on:consider={onConsider}
+	on:finalize={onFinalize}
 >
-	{#if item}
+	{#if _item}
 		<Letter>
-			{item.letter}
+			{_item.letter}
 		</Letter>
 	{/if}
 </div>
