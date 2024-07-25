b318d1ead781888317755332fb32c1fb6723a6f7
Zachary Stence <zachstence@gmail.com>
2024-07-24 21:16:57 -0500

    fix: check must use lowercase word

--- src/routes/check/[word]/+server.ts
+++ src/routes/check/[word]/+server.ts
@@ -2,8 +2,8 @@ import type { RequestHandler } from './$types';
 import { dictionaries } from '@scrabble-solver/dictionaries';
 import ScrabbleSolverTypes from '@scrabble-solver/types';
 
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = async ({ params: { word } }) => {
 	const trie = await dictionaries.get(ScrabbleSolverTypes.Locale.EN_US);
-	const isValid = trie.has(params.word);
+	const isValid = trie.has(word.toLowerCase());
 	return new Response(JSON.stringify({ isValid }));
 };
