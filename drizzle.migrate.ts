ef60afd0e1e8efd35b0afd944006b45a34c1d1d5
Zachary Stence <zachstence@gmail.com>
2025-01-08 22:28:28 -0600

    feat: use env for db connection

--- drizzle.migrate.ts
+++ drizzle.migrate.ts
@@ -1,12 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
+const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
+
+if (!POSTGRES_HOST) throw new Error('Missing required env POSTGRES_HOST');
+if (!POSTGRES_PORT) throw new Error('Missing required env POSTGRES_PORT');
+if (!POSTGRES_DB) throw new Error('Missing required env POSTGRES_DB');
+if (!POSTGRES_USER) throw new Error('Missing required env POSTGRES_USER');
+if (!POSTGRES_PASSWORD) throw new Error('Missing required env POSTGRES_PASSWORD');
+
 export const client = postgres({
-	host: 'localhost',
-	user: 'postgres',
-	password: 'password',
-	port: 5432
+	host: POSTGRES_HOST,
+	port: parseInt(POSTGRES_PORT),
+	db: POSTGRES_DB,
+	user: POSTGRES_USER,
+	password: POSTGRES_PASSWORD
 });
 
 export const db = drizzle(client);
--- src/lib/server/db/db.ts
+++ src/lib/server/db/db.ts
@@ -1,13 +1,23 @@
+import { env } from '$env/dynamic/private';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 import * as schema from './schema';
 
+const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } = env;
+
+if (!POSTGRES_HOST) throw new Error('Missing required env POSTGRES_HOST');
+if (!POSTGRES_PORT) throw new Error('Missing required env POSTGRES_PORT');
+if (!POSTGRES_DB) throw new Error('Missing required env POSTGRES_DB');
+if (!POSTGRES_USER) throw new Error('Missing required env POSTGRES_USER');
+if (!POSTGRES_PASSWORD) throw new Error('Missing required env POSTGRES_PASSWORD');
+
 const client = postgres({
-	host: 'db',
-	user: 'postgres',
-	password: 'password',
-	port: 5432
+	host: POSTGRES_HOST,
+	port: parseInt(POSTGRES_PORT),
+	db: POSTGRES_DB,
+	user: POSTGRES_USER,
+	password: POSTGRES_PASSWORD
 });
 
 export const db = drizzle(client, { schema });
